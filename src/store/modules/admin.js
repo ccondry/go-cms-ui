0a156484d62a4bf72c0de2b6fd13fbc3d89aa11c
Coty Condry <ccondry@cisco.com>
2020-10-19 23:21:54 -0500

    initial commit

---
+++ src/store/modules/admin.js
@@ -0,0 +1,22 @@
+import * as types from '../mutation-types'
+
+const state = {
+  users: []
+}
+
+const getters = {
+  users: state => state.users
+}
+
+const mutations = {
+}
+
+const actions = {
+}
+
+export default {
+  state,
+  actions,
+  getters,
+  mutations
+}
---
+++ src/store/mutation-types.js
@@ -0,0 +1,7 @@
+// loading and working states
+export const SET_WORKING = 'SET_WORKING'
+export const SET_LOADING = 'SET_LOADING'
+
+// auth
+export const SET_JWT = 'SET_JWT'
+export const SET_USER = 'SET_USER'
